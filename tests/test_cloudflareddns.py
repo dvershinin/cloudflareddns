02559891da5f0e5a1789d8e2bae09c89bb3ab192
Danila Vershinin <ciapnz@gmail.com>
2019-08-09 10:11:08 +0300

    Code cleanup

--- tests/test_cloudflareddns.py
+++ tests/test_cloudflareddns.py
@@ -3,6 +3,7 @@ from faker import Faker
 import time
 import socket
 from cloudflareddns import cloudflareddns
+import platform
 
 # change dir to tests directory to make relative paths possible
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
@@ -12,7 +13,11 @@ def test_update():
 
     cf_username = os.environ['CF_EMAIL']
     cf_key = os.environ['CF_KEY']
-    hostname = os.environ['TEST_HOSTNAME']
+    # hostname should be Python version specific so that different versions tests
+    # don't jump onto each other, so test271.example.com, test368.example.com, etc.
+    hostname = 'python{}.cloudflareddns.test.{}'.format(
+        platform.python_version(),
+        os.environ['CLOUDFLAREDDNS_TEST_DOMAIN'])
 
     faker = Faker()
     ip = faker.ipv4()
@@ -22,7 +27,7 @@ def test_update():
     # cf_username, cf_key, hostname, ip, proxied=False
     cloudflareddns.update(cf_username, cf_key, hostname, ip, False)
 
-    time.sleep(600)
+    time.sleep(180)
 
     # fetch record
     new_ip = socket.gethostbyname(hostname)
